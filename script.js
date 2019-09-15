0c48554374b5ae0b3edb93522f16fe89b974213c
RadekKosiada <kosiadar@gmail.com>
2019-09-15 12:59:55 +0200

    Render data on the page

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const mainContainer = $("#main-container");
-console.log(mainContainer)
-mainContainer.css({"border": "3px solid red"});
+console.log(mainContainer);
 
 $.ajax({
     type: 'GET',
     url: "data.json",
     contentType: 'application/json',
-    success: function(result) {
-        console.log(result)
+    success: function (result) {
+        console.log(result.aktuelles)
+        const current = result.aktuelles; //array
+        const events = result.veranstaltungen; //array
+
+        fillData(current);
+
+       fillData(events);
     }
 });
+
+
+function fillData (element) {
+    element.forEach(function (item, index) {
+        let currentContainer = mainContainer.append(`
+                <div class="current-container">
+                    <img src=${item.img}>
+                    <p>${item.date}</p>
+                    <h1 class="current">${item.headline}</h1>
+                    <p>${item.infos}</p>
+                    <p>${item.moreInfos}</p>
+                </div>`)
+    })
+}
